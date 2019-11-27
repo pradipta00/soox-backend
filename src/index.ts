e9483d86953dfa98591a7ebf6ec4de69e1ea409e
Pradipta00 <Pradipta2699@gmail.com>
2019-11-27 17:58:20 +0700

    modified main src

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from 'cors'
 import bodyParser from 'body-parser'
+// import { authRouter, dataRouter, uploadRouter } from '../3. router'
 
 const app = express();
 const port = 8080;
@@ -11,5 +12,9 @@ app.use(bodyParser());
 app.use('/music', express.static('5. files/music'))
 app.use('/payment', express.static('5. files/payment'))
 app.use('/thumbnails', express.static('5. files/thumbnails'))
+
+// app.use('/auth', authRouter)
+// app.use('/data', dataRouter)
+// app.use('/upload', uploadRouter)
 
 app.listen( port, _ => console.log( `Server running on => http://localhost:${ port }` ));
