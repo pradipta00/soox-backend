14ba2778f9f03ce34096ef993ab185fba82f2d84
Pradipta00 <Pradipta2699@gmail.com>
2019-12-02 18:03:01 +0700

    new control

--- 2. control/index.ts
+++ 2. control/index.ts
@@ -27,3 +27,56 @@ interface user {
 interface data {
     read : (_:{ query:{ id:number, table:string, albumId:number } }, __:res ) => void
 }
+
+const auth:auth = 
+    {
+        login : ( { body } , res) => {
+            let username = body.Username || '',
+                password = body.Password || '',
+                hashed:string = createHash(password),
+                query:string = `SELECT * FROM users WHERE username = ${username}`
+
+            if ( username.match( /[@]{1}[\w\d]+[\.]/g ) ) query = `SELECT * FROM users WHERE email = ${username}`
+
+            db.query( query, (err:{}, response:[{ password:string }]) => {
+
+                if (err) console.error(err)
+
+                if ( response.length && response[0].password === hashed ) res.send({ logged : true, ...response[0] })
+                else res.send({ logged : false })
+
+            })
+        },
+
+        newToken : ( { body }, res ) => {
+            let token = jwt.sign( body, secretKey, { expiresIn : expiredDuration } )
+                
+            res.send(token)
+        },
+        
+        verifyToken : ( { headers }:any, res ) => {
+            jwt.verify( headers.authorization, secretKey, (err:any, decoded:any) => {
+                if (err) res.send({ error : true, message : 'User Not Authorized' })
+                else res.send(decoded)
+            })
+        },
+
+        refreshToken : ( { headers }:any, res ) => {
+            jwt.verify( headers.authorization, secretKey, (err:any, decoded:{ id:number } ) => {
+                if (err) 
+                    res.send({ error : true, message : 'User Not Authorized' })
+                
+                let query = `SELECT * FROM users WHERE id = ${decoded.id}`
+                db.query( query, ( err, response:[{ password:string }] ) => {
+
+                    if (err) console.error(err)
+                    delete response[0].password
+
+                    res.send([
+                        response[0],
+                        jwt.sign( {...response[0]}, secretKey, { expiresIn : expiredDuration } )
+                    ])
+                })
+            })
+        },
+    }
